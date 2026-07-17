eac3b48248aea2f37cbb3ff9d7976e956d2ff624
.Ashish Bhandari. <ashishbhandari380@gmail.com>
2026-07-17 20:37:53 +0545

    changes

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('add/', views.add_user, name='add_user'),
     path('user-login/', views.user_login, name='user_login'),#user login url
     path('view/',views.all_user, name='view'),#view all users by admin
-    path('user-details/<str:user_id>/', views.get_user_details, name='user_profile'),#user profile view and update
+    path('user-details/<str:user_id>/', views.get_user_details, name='user_profile'),#user profile view and upda
     
 ]
